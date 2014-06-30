10802cdea2171adea41eec3fff4860ef64f1b36a
sasarky <sasarky@gmail.com>
2014-07-01 01:48:33 +0900

    helm conf

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -18,11 +18,17 @@
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "M-r") 'helm-resume)
 (define-key global-map (kbd "C-x C-u") 'helm-for-files)
-;; helm-map
-(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
 
+(eval-after-load 'helm
+  '(progn
+     (define-key helm-map (kbd "C-h") 'delete-backward-char)))
 
-(eval-after-load "helm-files"
-  '(setq helm-for-files-preferred-list (delete 'helm-source-locate helm-for-files-preferred-list)))
+(eval-after-load 'helm-files
+  '(progn
+     (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+     (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+     )
+  )
 
 (push '("*helm*") popwin:special-display-config)
